e2ffc66adc422f6081114f31805820cecfd4e4a4
Desiree Morris <desiquinncompany@gmail.com>
2019-06-13 19:06:45 -0400

    added objects for each class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -46,11 +46,11 @@ class Student extends Person {
     };
 
     PRAssignment(subject) {
-        console.log(`${student.name} has submitted a PR for ${subject}`)
+        console.log(`${this.name} has submitted a PR for ${subject}`)
     };
 
     sprintChallenge(subject) {
-        console.log(`${student.name} has begun sprint challenge on ${subject}`)
+        console.log(`${this.name} has begun sprint challenge on ${subject}`)
     };
 };
 
@@ -64,16 +64,85 @@ class PMs extends Instructor {
     };
 
     standUp(channel) {
-        console.log(`${PMs.name} announces to ${channel}, @channel standy times!​​​​​`);
+        console.log(`${this.name} announces to ${channel}, @channel standy times!​​​​​`);
     };
 
     debugsCode(student, subject) {
-        console.log(`${PMs.name} debugs ${student.name}'s code on ${subject}`);
+        console.log(`${this.name} debugs ${student.name}'s code on ${subject}`);
     };
 
 };
 
 const hermione = new Student({
-    
-})
+    name: 'Hermione Granger', 
+    location: 'London', 
+    age: 13,
+    previousBackground: 'Muggle Born',
+    className: 'Gryffindor',
+    favSubjects: {
+       0: 'potions',
+       1: 'broom riding', 
+       2: 'history of magic',
+    },
+});
 
+const george = new Student({
+    name: 'George Weasley', 
+    location: 'London', 
+    age: 13,
+    previousBackground: 'Witch Family',
+    className: 'Gryffindor',
+    favSubjects: {
+       0: 'lunch',
+       1: 'broom riding', 
+       2: 'defense against the dark arts',
+    },
+});
+
+const harry = new Student({
+    name: 'Harry Potter', 
+    location: 'London', 
+    age: 13,
+    previousBackground: 'The Boy Who Lived',
+    className: 'Gryffindor',
+    favSubjects: {
+       0: 'divinations',
+       1: 'broom riding', 
+       2: 'defense against the dark arts',
+    },
+});
+
+const snape = new Instructor({
+    name: 'Severous Snape',
+    location: 'Diagon Alley',
+    age: 38,
+    favLanguage: 'JavaScript',
+    specialty: 'Potions',
+    catchPhrase: 'I can teach you how to bottle fame, brew glory, even stopper death...',
+});
+
+const minerva = new Instructor({
+    name: 'Minerva McGonagall',
+    location: 'hogsmeade',
+    age: 45,
+    favLanguage: 'CSS5',
+    specialty: 'Transfiguration',
+    catchPhrase: 'We teachers are rather good at magic you know.',
+});
+
+const percy = new PMs({
+    name: 'Percy Weasley',
+    location: 'London',
+    age: 18, 
+    gradClassName: 'Class of 1994',
+    favInstructor: 'Prof. McGonagall',
+});
+
+console.log(hermione.name);
+console.log(george.favSubjects);
+console.log(harry.previousBackground);
+console.log(snape.catchPhrase);
+console.log(minerva.specialty);
+console.log(percy.standUp('Fun Channel'));
+console.log(percy.debugsCode(hermione, 'Postions'));
+console.log('favorite subjects' + hermione.listsSubjects);
